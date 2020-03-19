61a52b075f1944ee9b6f43f304f3f96ca50bcc82
umb0000 <sandwich1038@gmail.com>
2020-03-19 21:03:35 +0900

    deleted main

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require("express");
+import express from "express";
+import "core-js";
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on http://localhost:${PORT}`);
-} 
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res){
-    res.send("Hello from home");
+const handleHome = (req, res) => res.send("Hello from my home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("between");
+  next();
 }
-function handleProfile(req,res){
-    res.send("You are on my profile");
-}
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening); //4000port 
+
+app.listen(PORT, handleListening);
