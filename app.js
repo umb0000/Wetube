48db9239105a527db55e037c103c1a805cca41b3
umb0000 <sandwich1038@gmail.com>
2020-03-20 14:58:49 +0900

    index 제외 #2.9, 오류 있을 수 있음

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "core-js";
+import logger from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from my home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.get("/video", videoRouter);
+
+
+export default app;
